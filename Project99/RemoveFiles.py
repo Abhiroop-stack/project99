074d24a879b84c8be8126dd8f106c7e3ee578edb
Abhiroop-stack <72975864+Abhiroop-stack@users.noreply.github.com>
2021-12-13 14:30:32 +0530

    Add files via upload

---
+++ Project99/RemoveFiles.py
@@ -0,0 +1,12 @@
+import time
+import shutil
+import os
+
+path = ("F:\Abhiroop\Project99")
+days = 10
+seconds=time.time()
+
+if os.path.exists(path):
+    os.join(destination,path)
+    else:
+        os.remove(path)
